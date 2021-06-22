011da326de6ef872bbf6f39a7a25c496517f5192
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 10:43:16 -0300

    Funções Estudo

---
+++ Executando/Funções.lua
@@ -0,0 +1,10 @@
+-- 1 >
+print("Print � uma Fun��o.")
+-- 2 >
+print(" Todo texo � reconhecivel apenas ente �pas ou seja, se voce colocar o texto Ol� Mundo sem as aspas a fun��o ir� retornar erro. ")
+-- 3 >
+print("Uma fun��o sempre seguir� de parentes ent�o para atribuir um valor a essa fun��o que no caso seria nosso texto nos o colocamos entre parentes.. () ")
+-- 4 >
+print("� possivel citar n�meros e n�o h� necessidade de utilzar aspas.")
+
+print(123456789)
