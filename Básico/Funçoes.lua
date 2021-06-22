4cb86ce1043d2396ea8bad9f47ee14ee5ce713f2
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 08:59:31 -0300

    Execução dos Codifos via Lua

--- Básico/Funçoes.lua
+++ Básico/Funçoes.lua
@@ -1,9 +1,9 @@
--- Primeiramente utilzaremos uma função da propria linguagem lua para explicar com funciona uma função por mais básica que seja ela..
+-- Primeiramente utilizaremos uma função da propria linguagem lua para explicar com funciona uma função por mais básica que seja ela..
 
 
 -- 1 > Print é a nossa função 
--- 2 > Todo texo é reconhecivel apenas ente ápas ou seja, se voce colocar o texto Olá Mundo sem as aspas a função irá retornar erro.  
---   > Irá retornar erro porque ele não consegue reconher oque está dentro dos parentes, ele só reconhece como texo apenas entras aspas "".
+-- 2 > Todo texo é reconhecivel apenas entre áspas ou seja, se voce colocar o texto Olá Mundo sem as aspas a função irá retornar erro.  
+--   > Irá retornar erro porque ele não consegue reconhecer oque está dentro dos parentes, ele só reconhece como texo apenas entras aspas "".
 -- 3 > Uma função sempre seguirá de parentes então para atribuir um valor a essa função que no caso seria nosso texto nos o colocamos entre parentes.. () 
 -- 4 > Essa função "Print()" ela retrona o valor expecificado dentro dos parentes para o usuário.. No caso abaixo está retornando "Olá Mundo".. 
     
---
+++ Básico/Operadores-Matématicos.lua
@@ -0,0 +1,8 @@
+-- Eu sou pessimo em exatas, mas programar me ajudou a entender e compreender um pouco dela.. Principalmente utilizando esses operadores.
+
+-- 1 > Ainda trabalhando com a Função Print iremos realizar calculos básicos.
+
+print(A seguir vamos somar 1 + 1: )
+
+print(1 + 1)
+
