864cc937ccc98b548c97eca9545b9441cc29238c
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 10:42:41 -0300

    Trabalhando Operadores

--- Básico/Operadores-Matématicos.lua
+++ Básico/Operadores-Matématicos.lua
@@ -1,8 +1,13 @@
 -- Eu sou pessimo em exatas, mas programar me ajudou a entender e compreender um pouco dela.. Principalmente utilizando esses operadores.
 
 -- 1 > Ainda trabalhando com a Função Print iremos realizar calculos básicos.
+-- 2 > Nessa linguagem é bem fácil trabalhar com os algorismos matemáticos ( +, -, /, *, >, <, <=, >=, %)..
 
-print(A seguir vamos somar 1 + 1: )
+print("A seguir vamos realizar contas de matemática")
 
-print(1 + 1)
 
+print("1 + 1") -- Errado
+ou ?
+print(1 + 1) -- Certo
+
+print("Vídeo >  ")  
