89573594cdab9ab43281dc4e552f5da18a10b6df
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 07:52:23 -0300

    Função Print Básico

---
+++ Básico/Funçoes.lua
@@ -0,0 +1,35 @@
+-- Primeiramente utilzaremos uma função da propria linguagem lua para explicar com funciona uma função por mais básica que seja ela..
+
+
+-- 1 > Print é a nossa função 
+-- 2 > Todo texo é reconhecivel apenas ente ápas ou seja, se voce colocar o texto Olá Mundo sem as aspas a função irá retornar erro.  
+--   > Irá retornar erro porque ele não consegue reconher oque está dentro dos parentes, ele só reconhece como texo apenas entras aspas "".
+-- 3 > Uma função sempre seguirá de parentes então para atribuir um valor a essa função que no caso seria nosso texto nos o colocamos entre parentes.. () 
+-- 4 > Essa função "Print()" ela retrona o valor expecificado dentro dos parentes para o usuário.. No caso abaixo está retornando "Olá Mundo".. 
+    
+print("Olá Mundo") 
+
+-- > Nesse caso ele retornaria "Quero Aprender a Programar".. o valor atribuido a nossa função print é esse texto, citado dentro das aspas..
+
+print("Quero Aprender a Programar")
+
+-- ? > Posso colocar números dentro dessa função?
+-- > Sim, pode
+
+-- Mas nos casos dos números nos podemos atribuir ele de uma forma diferente, não precisa citar eles dentro das aspas..
+
+print(123456789)
+
+-- Porque disso? Em qualquer linguagem número é atribuido a seu valor, ele não é algo especial é apenas numeros diferentes dos textos.. 
+-- Textos são ilegiveis fora das áspas.. já números não.
+
+-- > Agora iremos resumir tudo em que aprendemos.
+
+-- 1 >
+print("Print é uma Função.")
+-- 2 >
+print(" Todo texo é reconhecivel apenas ente ápas ou seja, se voce colocar o texto Olá Mundo sem as aspas a função irá retornar erro. ")
+-- 3 >
+print("Uma função sempre seguirá de parentes então para atribuir um valor a essa função que no caso seria nosso texto nos o colocamos entre parentes.. () ")
+-- 4 >
+print("É possivel citar números e não há necessidade de utilzar aspas.") => print(123456789)
