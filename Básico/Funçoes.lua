9822eb380e583f849e3e94db9e76fc6cd4320d78
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 10:44:05 -0300

    main

--- Básico/Funçoes.lua
+++ Básico/Funçoes.lua
@@ -32,4 +32,6 @@ print(" Todo texo é reconhecivel apenas ente ápas ou seja, se voce colocar o t
 -- 3 >
 print("Uma função sempre seguirá de parentes então para atribuir um valor a essa função que no caso seria nosso texto nos o colocamos entre parentes.. () ")
 -- 4 >
-print("É possivel citar números e não há necessidade de utilzar aspas.") => print(123456789)
+print("É possivel citar números sem a necessidade de utilzar aspas.") => print(123456789) 
+
+Vídeo > 
