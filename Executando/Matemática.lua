07b66952b8aca414380ca88f0a8ca46b2c6231e2
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-22 10:43:58 -0300

    Matemática Estudo

---
+++ Executando/Matemática.lua
@@ -0,0 +1,49 @@
+
+-- Operadores Matem�ticos
+
+-- Adi��o ( + )
+print("Contas de Adi��o:")
+print(5 + 5)
+print(3 + 3)
+print(2 + 2)
+
+-- Subtra��o ( - )
+print("Contas de Subtra��o:")
+print(2 - 2)
+print(7 - 5)
+print(4 - 4)
+
+
+-- Divis�o ( / )
+print("Contas de Divis�o")
+print(4 / 2)
+print(90 / 8)
+print(9 / 5)
+
+
+-- Multiplica��o ( * )
+print("Contas de Multiplica��o")
+print(3 * 3)
+print(5 * 5)
+print(7 * 5)
+
+-- Operadores de Compara��o ( > Maior que ) ( < Menor que ) ( >= Maior Igual) e ( >= Menor Igual)
+print("Operadores de Compara��o")
+print(3 > 2)
+print(2 > 4)
+print(2 > 1)
+
+print(2 < 1)
+print(3 < 33)
+print(55 < 233)
+
+print("Operadores de Compara��o por =")
+print( 3 >= 2 )
+print( 2 <= 5)
+
+-- Operadores de Divis�o N�o Exatas ( Oque Resta )..
+print( 5 % 5)
+print( 9 % 2)
+print(3343434 % 23423233)
+
+
